6530c2e3e2aa2ff4c46d9c1b8cd4049a11fbf34d
Ajeet <ajeet.2040@gmail.com>
2020-11-20 20:05:41 +0530

    Added input parameters

--- src/main.py
+++ src/main.py
@@ -3,17 +3,27 @@
 import os
 import json
 import platform
+import argparse
+
+parser = argparse.ArgumentParser(description='Get child files and directory details in the json format as specified in Readme')
+parser.add_argument('input_path', help='Path of the input directory')
+parser.add_argument('--output_json_path',"-o", default='result.json', help='Output json path with file name. Default is "result.json" in current directory.')
 
 # TO DO
 # 0. Add additional file data as required -- Completed
-# 1. Add exception handling
-# 2. Add documentation
+# 1. Add exception handling -- Completed
+# 2. Add documentation -- Completed
 # 3. Code refactoring as applicable - naming conventions, modularity etc.
 # 4. Add logs , print statments
-# 5. Add usage message
+# 5. Add usage message and run parameters -- Completed
 # 6. Update Readme as applicable
+# 7. Next Version - Do packaging
 
-def get_file_folder_id(file_path):
+def get_file_directory_id(file_path):
+    """
+    Gets system id of file or directory
+    @param: file_path: File or directory absolute path
+    """
     return os.stat(file_path, follow_symlinks=False).st_ino
 
 def creation_date(path_to_file):
@@ -21,6 +31,7 @@ def creation_date(path_to_file):
     Try to get the date that a file was created, falling back to when it was
     last modified if that isn't possible.
     See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    @param: path_to_file: absolute file path whose created date is required.
     """
     if platform.system() == 'Windows':
         return os.path.getctime(path_to_file)
@@ -34,8 +45,15 @@ def creation_date(path_to_file):
             return stat.st_mtime
 
 def append_record_in_output_json(output_json, file_path, type, parent_id):
-    record_dict = {}
-    record_dict["id"] =  get_file_folder_id(file_path)
+    """
+    Saves the json file at the specified location.
+    @param: output_json: List where oytput is stored
+    @param: file_path: File or directory absolute path
+    @param: type: directory or file or root
+    @param: parent_id: system id of parent
+    """
+    record_dict = {}    # temp dict of file or directory details
+    record_dict["id"] =  get_file_directory_id(file_path)
     record_dict["name"] = os.path.basename(file_path)
     record_dict["type"] = type
     record_dict["parent_id"] = parent_id
@@ -43,38 +61,53 @@ def append_record_in_output_json(output_json, file_path, type, parent_id):
         record_dict["size"] = os.path.getsize(file_path)
     record_dict["modified_at"] = os.path.getmtime(file_path)
     record_dict["created_at"] = creation_date(file_path)
+    # Uncomment if need other details from os.stat
     # record_dict["stats"] = os.stat(file_path)
     output_json.append(record_dict)
     return output_json
 
-def save_json_file(dest_file_path, json_data):
-    with open(dest_file_path, 'w') as f:  # writing JSON object
+def save_json_file(output_json__path, json_data):
+    """
+    Saves the json file at the specified location.
+    @param: output_json__path: Json file path
+    @param: json_data: JSON data to be saved in file
+    """
+    with open(output_json__path, 'w') as f:  # writing JSON object
         json.dump(json_data, f)
 
-def main_func(root_path):
-    print("Hey bro", root_path)
-    output_json = []    # List of file/directory structure in json format
-    # Remove in case root path ends with single forward slash or 2 double slash or else handle in below finding name case
-    # Adding root folder
-    output_json = append_record_in_output_json(output_json, root_path, "ROOT", None)
-    # OS walk through the root directory
-    for root, dirs, files in os.walk(root_path):
-        # Get Parent id
-        parent_id = get_file_folder_id(root)
-        # Child directories
-        for dir in dirs:
-            print(dir)
-            dir_path = os.path.join(root,dir)
-            output_json = append_record_in_output_json(output_json, dir_path, "DIRECTORY", parent_id)
-        # Child files 
-        for file in files:
-            print(file)
-            file_path = os.path.join(root,file)
-            output_json = append_record_in_output_json(output_json, file_path, "FILE", parent_id)
-    # Processing completed
-    print("Processing is completed. Saving Json file")
-    save_json_file("result.json", output_json)
+def main_func(input_path, output_json_path):
+    """
+    Main function of processing. Uses os walks to get the directory and file details and saves it to json file.
+    @param: input_path: Input root path whose directory and file details are rerquired.
+    @param: output_json_path: Output JSON file path with name
+    """
+    try:
+        print("Processing started for input directory:", input_path, " .....")
+        output_json = []    # List of file/directory structure in json format
+        # Remove in case root path ends with single forward slash or 2 double slash or else handle in below finding name case
+        # Adding root directory
+        output_json = append_record_in_output_json(output_json, input_path, "ROOT", None)
+        # OS walk through the root directory
+        for root, dirs, files in os.walk(input_path):
+            # Get Parent id
+            parent_id = get_file_directory_id(root)
+            # Child directories
+            for dir in dirs:
+                dir_path = os.path.join(root,dir)
+                output_json = append_record_in_output_json(output_json, dir_path, "DIRECTORY", parent_id)
+            # Child files 
+            for file in files:
+                file_path = os.path.join(root,file)
+                output_json = append_record_in_output_json(output_json, file_path, "FILE", parent_id)
+        # Processing completed
+        print("Processing is completed. Saving json file at location: ", output_json_path)
+        save_json_file(output_json_path, output_json)
+    except Exception as e:
+        print("Some error occured: ", str(e))
+
    
 if __name__ == "__main__":
-    root_path = "C:/Users/ajeet/Local Drive/"
-    main_func(root_path)
+    args = parser.parse_args()
+    input_path = args.input_path
+    output_json_path = args.output_json_path
+    main_func(input_path, output_json_path)
